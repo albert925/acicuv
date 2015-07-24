12f94b2b1e2fc5aaea3f9273c998473bba12d6dd
albert925 <albertarias925@gmail.com>
2015-07-24 11:47:41 -0500

    acicuv

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+	include 'config.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, maximun-scale=1" />
+	<meta name="description" content="Somos la Enidad Financiera del Departamento 
+	Norte de Santander encargada de generar desarrollo económico y social 
+	mediante la prestación de servicios financieros a los entes territoriales, 
+	institutos descentralizados y empresas sociales del estado E.S.E" />
+	<meta name="keywords" content="Finacioero, prestacion de servicios" />
+	<title>Acicuv</title>
+	<link rel="icon" href="imagenes/icono.png" />
+	<link rel="image_src" href="imagenes/logo.png" />
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/iconos/style.css" />
+	<link rel="stylesheet" href="css/default/default.css" />
+	<link rel="stylesheet" href="css/nivo_slider.css" />
+	<link rel="stylesheet" href="css/owl_carousel.css" />
+	<link rel="stylesheet" href="css/owl_theme_min.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/inicio.css" />
+	<script src="js/jquery_2_1_1.js"></script>
+	<script src="js/owl_carousel_min.js"></script>
+</head>
+<body>
+	<header>
+		<article id="redes">
+			<a href="" target="_blank"><span class="icon-facebook"></span></a>
+			<a href="" target="_blank"><span class="icon-twitter"></span></a>
+			<a href="" target="_blank"><span class="icon-instagram"></span></a>
+			<a href="" target="_blank"><span class="icon-youtube3"></span></a>
+		</article>
+	</header>
+	<article id="cajmn">
+		<div id="mn_mv"><span class="icon-menu"></span></div>
+		<nav id="mnP">
+			<ul>
+				<li><a href="">Inicio</a></li>
+				<li><a href="quejas">Quejas</a></li>
+				<li><a href="nosotros">Quienes somos</a></li>
+				<li><a href="pasando">Que está pasando</a>
+					<ul>
+						<li><a href="#">Menu1</a></li>
+					</ul>
+				</li>
+				<li><a href="sitios">Sitios de interés</a></li>
+				<li><a href="columnistas">Columnistas</a></li>
+				<li><a href="contacto">Contáctenos</a></li>
+			</ul>
+		</nav>
+	</article>
+	<article>
+		<figure></figure>
+		<article></article>
+	</article>
+	<section>
+	</section>
+	<footer></footer>
+</body>
+</html>
